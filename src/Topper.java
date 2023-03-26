e9aa47d43db709c3add01efc5c206312496623ea
Vignesh-1704 <vigneshmanikavasagam17@gmail.com>
2023-03-26 21:29:59 +0530

    Add. Included The Logic To Check If It Is A Probable Topper Number

--- src/Topper.java
+++ src/Topper.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Topper {
+    public static boolean isMatches(int oddRes , int evenRes)
+    {
+        return oddRes == evenRes;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number to find ODD and EVEN sum: ");
+        int number = scanner.nextInt();
+        int oddSum = 0;
+        int evenSum = 0;
+        while(number > 0)
+        {
+            int last = number % 10;
+            if(last % 2 == 0)
+            {
+                evenSum += last;
+            }
+            else
+            {
+                oddSum += last;
+            }
+            number = number / 10;
+        }
+        System.out.println("The Sum of Odd Digits is : "+ oddSum);
+        System.out.println("The Sum of Even Digits is : "+ evenSum);
+        boolean res = isMatches(oddSum,evenSum);
+        System.out.println("Probable Topper: "+ res);
     }
 }
